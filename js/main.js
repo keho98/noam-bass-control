5d0f6b54ad2afd1536d74b25e2d771af32e038b0
Kevin Ho <keho98@gmail.com>
2014-06-09 18:01:04 -0700

    Starts NoamBassControl

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+/* global $, _ */
 
+function NoamBassControl() {
+    this.threshold = 5;
+    this.accelX = null;
+    this.accelY = null;
+    this.accelZ = null;
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', this.handleAccelerometerChange.bind(this));
+    } else {
+        console.log("WARNING: devicemotion not supported");
+    }
+}
+
+NoamBassControl.prototype.handleAccelerometerChange = function(event) {
+    var x = event.acceleration.x,
+        y = event.acceleration.y,
+        z = event.acceleration.z;
+    if(this.isValidShake(x,y,z)) {
+        this.sendShake();
+    } else {
+        this.accelX = x;
+        this.accelY = y;
+        this.accelZ = z;
+    }
+};
+
+NoamBassControl.prototype.isValidShake = function(accelX, accelY, accelZ) {
+    return (this.accelX && this.accelY && this.accelZ) &&
+        (this.accelX - accelX > this.threshold || this.accelY - accelY > this.threshold || this.accelZ - accelZ > this.threshold);
+};
+
+NoamBassControl.prototype.sendShake = function() {
+    $.post('/', {
+        success: function() {
+            console.log("Post successful");
+        }
+    })
+};
+
+$(function() {
+    var bassControl = new NoamBassControl();
+});
